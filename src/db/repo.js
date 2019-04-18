3aaa748b4997d105033ed0f3ee51860cb7818ec0
nictengkk <nictengkk@gmail.com>
2019-04-18 16:07:55 +0800

    use parametrised query to prevent node SQL injection in password input area

--- src/db/repo.js
+++ src/db/repo.js
@@ -14,4 +14,5 @@ export const init = async () => {
 
 export const userValid = async (username, password) => {
   return await db.get(`SELECT username FROM users WHERE username = '${username}' AND password = '${password}';`);
+  // return await db.get(`SELECT username FROM users WHERE username = ? AND password = ?;`, username, password);
 }
